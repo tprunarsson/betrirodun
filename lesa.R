8d08ebf534dc39035b004ea1ad7695f00246d1e8
Thomas Philip Runarsson <tpr@hi.is>
2018-04-06 23:42:34 +0000

    ...

--- lesa.R
+++ lesa.R
@@ -2,7 +2,7 @@
 require(openxlsx)
 require(lubridate) # https://www.r-statistics.com/2012/03/do-more-with-dates-and-times-in-r-with-lubridate-1-1-0/
 
-
+setwd("~/projects/betrirodun")
 # Hvar eru skjölin og hvað heita þau?
 filepath = c("./gogn/")
 # Eyða "bilum" út nafni, nota hér "_" 
@@ -22,12 +22,30 @@ rm(list = c("O"))
 # Finna einkvæma aðgerðakort
 adgerdakort = unique(ORBIT$Aðgerðarkort)
 
+# Varpa N/A í nan
+for (n in names(ORBIT)) {
+  i = (ORBIT[n] == "N/A")
+  ORBIT[n][i] = NaN
+}
+
 # Búum til data frame fyrir hvert kort með upplýsingum ...
 adkort = list()
 for (a in adgerdakort) {
   i <- which(ORBIT$Aðgerðarkort == a)
-  dt <- hm(ORBIT$Svæfing.hefst[i]) - hm(ORBIT$Svæfingu.lýkur[i])
-  adkort[[adgerdakort[1]]] = data.frame(skurdtimi = c(1:10), undir=c(1:10))
+  Dagsetning <- ymd(convertToDateTime(ORBIT$Dagsetning.aðgerðar[i]))
+  InnASkurdgang <- Dagsetning + hm(ORBIT$Inn.á.skurðgang[i], quiet = TRUE)
+  InnAStofu <- Dagsetning + hm(ORBIT$Inn.á.stofu[i], quiet = TRUE)
+  SvaefingHefst <- Dagsetning + hm(ORBIT$Svæfing.hefst[i], quiet = TRUE)
+  AdgerdHefst <- Dagsetning + hm(ORBIT$Aðgerð.hefst[i], quiet = TRUE)
+  AdgerdLykur <- Dagsetning + hm(ORBIT$Aðgerð.lýkur[i], quiet = TRUE)
+  SvaefingLykur <- Dagsetning + hm(ORBIT$Svæfingu.lýkur[i], quiet = TRUE)
+  UtAfSkurdgangi <- Dagsetning + hm(ORBIT$Út.af.skurðgangi[i], quiet = TRUE)
+  InnAVoknun <- Dagsetning + hm(ORBIT$Inn.á.vöknun[i], quiet = TRUE)
+  UtAfVoknun <- Dagsetning + hm(ORBIT$Út.af.vöknun[i], quiet = TRUE)
+  adkort[[a]] = data.frame(Dagsetning, 
+                            InnASkurdgang,InnAStofu,SvaefingHefst, 
+                            AdgerdHefst,AdgerdLykur,SvaefingLykur,
+                            UtAfSkurdgangi,InnAVoknun,UtAfVoknun)
 }
   
 
