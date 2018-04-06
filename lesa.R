3f40a7c4c4a3e4b2c33f4842bf48743debe1c1f1
Thomas Philip Runarsson <tpr@Boas.hi>
2018-04-06 01:12:47 +0000

    ...

--- lesa.R
+++ lesa.R
@@ -1,5 +1,16 @@
 # Les Excel skjöl og mynda data.frame sem er notað í greiningu
+require(openxlsx)
 
 # Hvar eru skjölin og hvað heita þau?
-filepath = c("")
-filenames = c("")
+filepath = c("./gogn/")
+filename = c("_með_aðgerðakortum.xlsx")
+
+# Lestum eitt ár í einu:
+ORBIT = NULL
+for (ar in seq(2009,2010)) {
+  fname = paste0(filepath,as.character(ar),filename, sep="")
+  print(fname)
+  O <- read.xlsx(fname, sheet = "Skurðaðgerðir - ORBIT", startRow = 2, colNames = TRUE)
+  O <- O[-nrow(O),]                                     # remove last row, its not data
+  ORBIT = rbind(ORBIT,O)
+}
