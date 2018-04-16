957bc5f54168c529438f268b1b10c6fcc1fc4358
Ásgeir Örn Sigurpálsson <asgeirorns@gmail.com>
2018-04-16 10:26:59 +0000

    Update skrifa.R

--- skrifa.R
+++ skrifa.R
@@ -2,15 +2,23 @@
 rm(list=ls())
 options(encoding = "UTF-8") 
 
+#TODO:
 #Param roster - Aetlum vid ad hafa thad hardkodad?
+#Rett numer a bidlista
 
 #Importum pakka
 library(httr)
 library(jsonlite)
 library(dplyr)
 library(lubridate)
+library(tictoc)
+#pc heima
+setwd("~/ModelandData")
+#pc skola
+
 
 #---------------------------------Biðlistaþjónusta----------------------------------------#
+tic()
 
 # Lesa biðlista (nota þjónustu seinna)
 get_base_parsed <- fromJSON(txt="getWaitingList.json", flatten = TRUE)
@@ -28,19 +36,38 @@ get_base_df$OrbitOperation.OperationCard_PreTime <- as.numeric(get_base_df$Orbit
 Bidlisti = get_base_df[get_base_df$OrbitOperation.OperationSpecialty %in% c("Hb. Alm.", "Hb. Þvagf."),]
 
 
+
+
+
 #Rodum bidlistandum upp eftir sersviidi, laekni, priority af sjukling og svo skrasetningardagsetning a bidlista.
-#Gert til ad numera sjuklinga sidar
+#Gert til ad numera sjuklinga sidar - kannski er thetta ekki alveg must...
 Bidlisti <- Bidlisti[order(Bidlisti$OrbitOperation.OperationSpecialty,
                            Bidlisti$OrbitOperation.RequestedOperator_Name, 
                            Bidlisti$OrbitOperation.OperationPriority,
                            Bidlisti$OrbitOperation.RegistrDay),]
 
-#----------------------------------Skrifum gögn--------------------------------------#
 
+
+toc()
+#0.2 sec
+#----------------------------------Skrifum gögn--------------------------------------#
+tic()
 # skrifa í AMPL dat skrá allt sem þarf til að besta
 fname = c("kvid.dat")
 #Lesa söguleg gögn (sem lesa.R skrifar)
 load("adkort.Rdata")
+
+#----------------------------------Debug---------------------------------------------#
+#Ef adgerdarkort finnst ekki i sogulegum gognum viljum vid henda tvi ut? Hvad er edlilegt?
+#Her er thaer adgerdir sem finnast ekki i sogulegum gognum
+MissingData = Bidlisti[!Bidlisti$OrbitOperation.OperationCard %in% adkort$Adgerdakort,]
+#Uppfaerdur bidlisti
+Bidlisti = Bidlisti[Bidlisti$OrbitOperation.OperationCard %in% adkort$Adgerdakort,]
+
+
+
+
+
 
 #--------------------------------LAEKNAR-----------------------------------------------#
 # Skrifum niður lækna - thad er e-d sem heitir lika Surgeon..- veit ekki alveg muninn?
@@ -74,31 +101,56 @@ Adgerdarkort = Bidlisti$OrbitOperation.OperationCard
 Kennitala = Bidlisti$OrbitOperation.PatientSSN
 
 #Skrifum ut demand theas hvada sjuklingur og adgerd er per laekni
-cat("param Demand : ", file = "kvid.dat", sep = "", append=TRUE)
+#Finnst thetta frekar othaegileg framsetning a gognunum og erfid til yfirlestrar
+#Aetla ad utfaera odruvisi t.d.
+#param Demand :=
+#"0101010109-Kjálki, efri, Lefort I (HNE)" "Arnar Þór Guðjónsson" 1
+#Og sleppa ad prenta ef thad er 0 og setja thad i default.
+
+
+tic()
+cat("param Demand := \n", file = "kvid.dat", sep = "", append=TRUE)
 for (i in c(1:length(Bidlisti$OrbitOperation.PatientSSN))) {
   adgek = Adgerdarkort[i]
   kt = Kennitala[i]
-  cat(paste0('"',kt,'-',adgek,'" '), file="kvid.dat", sep = "\t", append=TRUE)
+  lak = Laeknir[i]
+  cat(paste0('"',lak,'"','\t','"',kt,'-',adgek,'" ','1'), file=fname, sep = "\n", append=TRUE)
 }
-cat(" := ", file = fname, sep = "\n", append=TRUE)
+cat(";", file = fname, sep = "\n", append=TRUE)
+toc()
+#1.03sec
 
-for (lak in uLaeknir) {
-  cat(paste0('"',lak,'"'), file=fname, sep = c(" "), append=TRUE)
-  # how many OR of type case
-  id = which(Laeknir == lak)
-  for (i in c(1:length(Adgerdarkort))) {
-    if (i %in% id)
-      cat(" 1 ", file=fname, sep = c(" "), append=TRUE)
-    else
-      cat(" 0 ", file=fname, sep = c(" "), append=TRUE)
-  }
-  cat("",file=fname, sep = "\n", append=TRUE)
-  
-}
-cat(";",file=fname, sep = "\n", append=TRUE)
+#Skrifum ut demand theas hvada sjuklingur og adgerd er per laekni
+#Finnst thetta frekar othaegileg framsetning a gognunum og erfid til yfirlestrar
+#Aetla ad utfaera odruvisi t.d.
+#param Demand :=
+#"0101010109-Kjálki, efri, Lefort I (HNE)" "Arnar Þór Guðjónsson" 1
+#Og sleppa ad prenta ef thad er 0 og setja thad i default.
 
+#tic()
+#cat("param Demand :", file=fname, sep="\n", append=TRUE)
+#for (i in c(1:length(Bidlisti$OrbitOperation.optillfalle_id))){
+#  adgek=Adgerdarkort[i]
+#  kt = Kennitala[i]
+#}
+#for (lak in uLaeknir) {
+#  cat(paste0('"',lak,'"'), file=fname, sep = c(" "), append=TRUE)
+#  # how many OR of type case
+#  id = which(Laeknir == lak)
+#  for (i in c(1:length(Adgerdarkort))) {
+#    if (i %in% id)
+#      cat(" 1 ", file=fname, sep = c(" "), append=TRUE)
+#    else
+#      cat(" 0 ", file=fname, sep = c(" "), append=TRUE)
+#  }
+#  cat("",file=fname, sep = "\n", append=TRUE)
+
+#}
+#cat(";",file=fname, sep = "\n", append=TRUE)
+#toc()
+#Thetta fall tekur 26sek!
 #---------------------------------DAGAR--------------------------------------------------#
-#Buum til runu af dagsetningum - user defined..
+#Buum til runu af dagsetningum - user defined.. 
 numberOfDays = 30
 StartDate = as.Date("2018-04-11")
 RodDaga<- seq(StartDate, by=1, length.out = numberOfDays)
@@ -133,6 +185,7 @@ cat(";", file=fname, sep="\n", append = TRUE)
 #480 min virka daga, man-fim
 #330 min a fostudogum,
 #0 min um helgar
+#Thurfum lika fridaga!!
 
 
 cat("param T := \n",file=fname, sep=" ", append=TRUE)
@@ -251,43 +304,41 @@ cat(";",file=fname, sep = "\n", append=TRUE)
 
 
 #Skrifum ut expectedWard fyrir hvern dag, viljum einungis tha sem eru merktir fyrir ad thurfa ward
-#Ath her vantar fallid fyrir likur a legu..thad heitir CodeExpectedWard[case,j]
 cat("param expectedWard : ",file=fname,sep="\n", append = TRUE)
 for (j in c(1:7))
   cat(sprintf(" %d ", j), file=fname, sep = "", append=TRUE)
 cat(":=",file=fname,sep="\n", append=TRUE)
-
 for (i in c(1:length(Adgerdarkort))) {
-  idx=which(Lega[i]==c("Legudeild"))
+  #We must also check if the operation card exist in our data
+  idx=which(Lega[i]==c("Legudeild") & Adgerdarkort[i] %in% adkort$Adgerdakort)
   if(length(idx)>0){
     adge=Adgerdarkort[i]
     kt = Kennitala[i]
     cat(paste0('"',kt,'-',adge,'"','\t',' 1'),file=fname ,sep="", append=TRUE)
-    #cat(sprintf(" o%03d_%s 1",i,case), file="kvid.dat", sep = "", append=TRUE)
     for (j in c(1:6))
-      cat(sprintf(" %.3f ",j), file="kvid.dat", sep =
+      cat(sprintf(" %.3f ",LeguLikur[adge,j]), file=fname, sep =
             "", append=TRUE)
     cat(" ",file=fname,sep="\n", append=TRUE)
   }
 }
 cat(";",file=fname,sep="\n", append=TRUE)
 
+
 #Skrifum ut expectedICU fyrir hvern dag, viljum einungis tha sem eru merktir fyrir ad thurfa ad fara i ICU utfra bidlista?
-#Ath her vantar fallid fyrir likur a legu..thad heitir CodeExpectedGjor[case,j]
 cat("param expectedICU : ",file=fname,sep="\n", append = TRUE)
 for (j in c(1:7))
   cat(sprintf(" %d ", j), file=fname, sep = "", append=TRUE)
 cat(":=",file=fname,sep="\n", append=TRUE)
 
 for (i in c(1:length(Adgerdarkort))) {
-  idx=which(YfirNottAICU[i]==c("1"))
+  idx=which(YfirNottAICU[i]==c("1") & Adgerdarkort[i] %in% adkort$Adgerdakort)
   if(length(idx)>0){
     adge=Adgerdarkort[i]
     kt = Kennitala[i]
     cat(paste0('"',kt,'-',adge,'"','\t',' 1'),file=fname ,sep="", append=TRUE)
     #cat(sprintf(" o%03d_%s 1",i,case), file="kvid.dat", sep = "", append=TRUE)
     for (j in c(1:6))
-      cat(sprintf(" %.3f ",j), file="kvid.dat", sep =
+      cat(sprintf(" %.3f ",GjorLikur[adge,j]), file="kvid.dat", sep =
             "", append=TRUE)
     cat(" ",file=fname,sep="\n", append=TRUE)
   }
@@ -301,74 +352,45 @@ cat(";",file=fname,sep="\n", append=TRUE)
 scenarios=10
 cat("param nscenarios :=",scenarios,";", file = "kvid.dat", sep = "\n", append=TRUE)
 
-#Thurfum adeins ad utfaera thetta betur?
+#Eg set her skilyrdid ad adgerdarkortid turfi ad vera til i gognunum..
 cat("param SurgeryTime := ",file="kvid.dat",sep="\n", append=TRUE)
-
 Scenario = c(1:scenarios)
-
-adkort2 <- adkort[order(-xtfrm(adkort$Dagsetning),adkort$Adgerdakort),] 
-
 for (i in c(1:length(Adgerdarkort))){
   adge = Adgerdarkort[i] #ur bidlista
   laeknir = Laeknir[i] #ur bidlista
-  kt = Kennitala[i]
-  idx = which(adkort2$Adgerdakort==adge & adkort2$Laeknir==laeknir
-              & adkort2$AdgerdaTimi<=24*60 & adkort2$AdgerdaTimi>0 & adkort2$Dagsetning>=Sys.Date()-(365*4))
+  kt = Kennitala[i] #ur bidlista
+  idx = which(adkort$Adgerdakort==adge & adkort$Laeknir==laeknir
+              & adkort$AdgerdaTimi<=24*60 & adkort$AdgerdaTimi>0 &adge %in% adkort$Adgerdakort)
   if(length(idx)<10){
-    #Notum tima fyrir annara ef thad finnst ekki annar - forum 4 ar aftur i timann
-    idx = which(adkort2$Adgerdakort==adge &
-                  adkort2$AdgerdaTimi<=24*60 & adkort2$AdgerdaTimi>0 & adkort2$Dagsetning>=Sys.Date()-(365*4))
+    #Notum tima  annara ef thad finnst ekki annar 
+    idx = which(adkort$Adgerdakort==adge &
+                  adkort$AdgerdaTimi<=24*60 & adkort$AdgerdaTimi>0 & adge %in% adkort$Adgerdakort)
   }
   idx <- rev(idx)
   idx <- idx[1:min(10,length(idx))]
   for (s in Scenario){
-    id = sample(idx,1)
-    cat(paste0('"',kt,'-', adge,'"','\t' ,'"', laeknir,'"','\t' ,s,'\t',
-               as.numeric(adkort2$AdgerdaTimi[id])), file="kvid.dat", sep = "\n",
-        append=TRUE)
+    
+    #Tokum 10 sidustu gildi
+    id= idx[s]
+    if(is.na(adkort$AdgerdaTimi[id])==FALSE){
+      
+      cat(paste0('"',kt,'-', adge,'"','\t' ,'"', laeknir,'"','\t' ,s,'\t',
+                 as.numeric(adkort$AdgerdaTimi[id])), file="kvid.dat", sep = "\n",
+          append=TRUE)}
+    else  #nota utreiknadann medaltima fyrir adgerdarkortid ur bidlista?
+      #hvad ef hann er ekki til?
+      cat(paste0('"',kt,'-', adge,'"','\t' ,'"', laeknir,'"','\t' ,s,'\t',round(Bidlisti$OrbitOperation.OperationCard_OpTime[i]*60)), file="kvid.dat", sep = "\n",
+          append=TRUE)
   }
 }
 
-
 cat(";",file="kvid.dat",sep="\n", append=TRUE)
 
+#Post og pre-operation times
+
+
+toc()
+#19.62sec
 
 
 
-
-# Skrifum niður verkefni (kt.)
-
-#OLD wdname = c("Sun", "Mon", "Tue", "Wed", "Thu", "Fri", "Sat")
-#OLD DaysName = sprintf("%s_%d_%d_%d ",wdname[wday(DAYS)],day(DAYS),month(DAYS),year(DAYS))
-#OLD nuDaysName = sprintf("%s_%d_%d_%d ",wdname[wday(NUDAYS)],day(NUDAYS),month(NUDAYS),year(NUDAYS))
-#OLD for (i in c(1:length(nuDaysName))) {
-#OLD   nuDaysName[i] <- sprintf("%d", which(nuDaysName[i] == DaysName)[1])
-#OLD }
-
-#OLD DaysName = sprintf("%d ", c(1:length(DAYS)));
-
-#OLD daybuffer = sprintf("%d ", 1+c(length(DAYS):(length(DAYS)+5)));
-
-#OLD cat("set Day := ",file="kvid.dat",sep="\n")
-#OLD for (d in DaysName) {
-#OLD   cat(d, file="kvid.dat", sep = "", append=TRUE)
-#OLD }
-#OLD for (d in daybuffer) {
-#OLD   cat(d, file="kvid.dat", sep = "", append=TRUE)
-#OLD }
-#OLD cat(";",file="kvid.dat", sep = "\n", append=TRUE)
-#OLD 
-#OLD cat("param isWeekend := ",file="kvid.dat",sep="\n", append=TRUE)
-#OLD for (i in c(1:length(DAYS))) {
-#OLD   d <- wday(DAYS)[i]
-#OLD   dn <- DaysName[i]
-#OLD   cat(dn, file="kvid.dat", sep = "", append=TRUE)
-#OLD   if (d == 1 | d == 7)
-#OLD     cat(" 1", file="kvid.dat", sep = "\n", append=TRUE)
-#OLD   else
-#OLD     cat(" 0", file="kvid.dat", sep = "\n", append=TRUE)
-#OLD }
-#OLD for (d in daybuffer) {
-#OLD   cat(sprintf("%s 1",d), file="kvid.dat", sep = "\n", append=TRUE)
-#OLD }
-#OLD cat(";",file="kvid.dat", sep = "\n", append=TRUE)S
